ac5d3f338b2e84f554901c1a86e52496fe0133e0
Lang Nguyen <thilang0608@gmail.com>
2025-11-06 08:07:30 +0700

    Add db_utils and transform_and_load_to_dw file

---
+++ src/utils/db_utils.py
@@ -0,0 +1,20 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def connect_to_db(db_name):
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv("MYSQL_HOST"),
+            port=os.getenv("MYSQL_PORT"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=db_name
+        )
+        print(f"Kết nối {db_name} thành công!")
+        return conn
+    except mysql.connector.Error as e:
+        print(f"Lỗi khi kết nối {db_name}: {e}")
+        return None
