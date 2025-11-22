a11e7da2abffdeb1a6acfae4356a241e9148ab5f
Lang Nguyen <thilang0608@gmail.com>
2025-11-22 16:46:22 +0700

    Adjust logic load to dwh with read from csv file

--- src/export_staging.py
+++ src/export_staging.py
@@ -1,4 +1,3 @@
-# export_staging.py
 import pandas as pd
 from utils.db_utils import connect_to_db
 
@@ -6,13 +5,13 @@ def export():
     conn = connect_to_db("news_staging_db")
     if not conn: return
     
-    print("📦 Đang xuất dữ liệu ra file CSV...")
+    print("Đang xuất dữ liệu ra file CSV...")
     # Lấy dữ liệu
     df = pd.read_sql("SELECT * FROM staging_delta", conn)
     
     # Xuất file (Không header)
     df.to_csv("delta_data.csv", index=False, header=False)
-    print(f"✅ Đã xuất {len(df)} dòng ra file: delta_data.csv")
+    print(f"Đã xuất {len(df)} dòng ra file: delta_data.csv")
     conn.close()
 
 if __name__ == "__main__":
